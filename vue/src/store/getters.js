f9218bb9fc322b1be05805e2efaaa8c4da2a75ae
黄凯伦 <340443366@qq.com>
2017-12-06 16:52:03 +0800

    update

--- vue/src/store/getters.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by kelen on 2017/2/28.
- */
-export default {
-    userInfo: (state) => state.userInfo,
-}
--- vue/src/store/mutations-types.js
+++
@@ -1,4 +0,0 @@
-export const GET_USERINFO = "getUserInfo"
-export const SET_USERINFO = "setUserInfo"
-export const SET_USERID = "setUserId"
-export const LOGOUT = "logout"
