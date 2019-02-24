d3b3e33ade687996651bf436bf0a3f52c0c49fbb
黄凯伦 <340443366@qq.com>
2019-02-24 11:47:58 +0800

    react-scaffold-demo

---
+++ react/precache-manifest.68d8f2a85420dac77e3b47bf8beadef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ef3de70d39b087d174ec",
+    "url": "/static/js/main.ef3de70d.chunk.js"
+  },
+  {
+    "revision": "e24f628b467e1f307a32",
+    "url": "/static/js/2.e24f628b.chunk.js"
+  },
+  {
+    "revision": "ef3de70d39b087d174ec",
+    "url": "/static/css/main.ea15191c.chunk.css"
+  },
+  {
+    "revision": "e24f628b467e1f307a32",
+    "url": "/static/css/2.32223125.chunk.css"
+  },
+  {
+    "revision": "985f5765c85737eb6a2a0495d4049832",
+    "url": "/index.html"
+  }
+];
